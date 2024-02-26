f326d8e12cd4ee7907daa35cee5af59e4758d978
dp9318 <dipankarkumar626@gmail.com>
2024-02-26 17:41:04 +0530

    Finished

--- app/src/main/java/com/dp/imagecard/MainActivity.kt
+++ app/src/main/java/com/dp/imagecard/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -29,9 +33,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val painter = painterResource(id= R.drawable.cat_new_res)
+            val painter2 = painterResource(id= R.drawable.cat)
             val description = "Cat on the grass!"
             val title = "Cat is sitting on the grass"
-            ImageCard(painter = painter, contentDescription = description, title = title)
+            val title2 = "Cat is standing on sofa"
+            Column (modifier = Modifier
+                .fillMaxWidth(.5f)
+                .padding(5.dp) ){
+                Box{
+                    ImageCard(painter = painter, contentDescription = description, title = title)
+
+                }
+                Box {
+                    ImageCard(painter = painter2, contentDescription = description, title = title2)
+
+                }
+            }
+
+
+
         }
     }
 }
@@ -50,8 +70,22 @@ fun ImageCard(
         Box (modifier = Modifier.height(200.dp)) {
             Image(painter = painter ,
                 contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .aspectRatio(1f)
             )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                ))
             Box(modifier = Modifier
                 .fillMaxSize()
                 .padding(5.dp),
